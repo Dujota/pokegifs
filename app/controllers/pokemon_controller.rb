ef1482c101bdf934bc7a734f2a50e26f695d51b4
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-08 01:08:33 -0500

    finished assignment, no stretch yet

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -9,6 +9,7 @@ class PokemonController < ApplicationController
     # id = pokemon_api_body["id"],
     # type = pokemon_api_body["types"].map { |type| type["type"]["name"] }}
 
+    # assign the output of the pokemon info to a variable to be called on when we render the json
     pokemon_info = {
       id: pokemon_api_body["id"],
       name: pokemon_api_body["name"],
@@ -17,6 +18,13 @@ class PokemonController < ApplicationController
              }
     }
 
-    render json: { pokemon: pokemon_info }
+    giphy_results = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{pokemon_api_body["name"]}&rating=g")
+
+    giphy_body = JSON.parse(giphy_results.body)
+
+    giphy_url = giphy_body["data"].sample["url"] # look at the data array, get a random object and get its url
+
+    # binding.pry
+    render json: { pokemon: pokemon_info, "gif": giphy_url } # creates a pokemon hash with the nested hash containing the polemon_info
   end
 end
