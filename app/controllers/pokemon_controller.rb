5ff74507f48bd6f91b43aaf623ed48baba19611e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-07 20:56:02 -0500

    displaying the pokemon info in a hash in controller using render json pokemon: pokemon_info

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,22 @@
 class PokemonController < ApplicationController
 
   def show
-    render json: { "message": "ok" }
+    pokemon_api_result = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    pokemon_api_body = JSON.parse(pokemon_api_result.body)
+    # binding.pry
+    # poke_data = {
+    # name = pokemon_api_body["name"],
+    # id = pokemon_api_body["id"],
+    # type = pokemon_api_body["types"].map { |type| type["type"]["name"] }}
+
+    pokemon_info = {
+      id: pokemon_api_body["id"],
+      name: pokemon_api_body["name"],
+      types: pokemon_api_body["types"].map { |type|
+               type["type"]["name"]
+             }
+    }
+
+    render json: { pokemon: pokemon_info }
   end
 end
